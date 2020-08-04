d54ed6a14f616ce99fc194fc1bf625eac801c21c
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-08-04 18:38:20 -0500

    test(integration-tests): add integration test for user creation

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Users', type: :request do
+  describe 'GET /api/v1/users' do
+    let(:user_params) do
+      { email: 'user@duetcode.io', password: 'samplepassword' }
+    end
+
+    it 'create a new user' do
+      post api_v1_users_path, params: { user: user_params }
+      expected_body = { 'email' => 'user@duetcode.io' }
+
+      expect(response).to have_http_status(:created)
+      expect(load_body(response)).to include(expected_body)
+    end
+
+    it 'returns unprocessable entity with errors' do
+      user_params[:password] = nil
+      post api_v1_users_path, params: { user: user_params }
+
+      expected_body = { 'password' => ['can\'t be blank'] }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(load_body(response)).to eq(expected_body)
+    end
+  end
+end
