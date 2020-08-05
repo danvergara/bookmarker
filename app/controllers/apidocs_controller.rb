9998a6436aa1c7489b5d3fb97a233c0a3b905637
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-08-05 14:31:32 -0500

    docs(users): add swagger documentation as json response

---
+++ app/controllers/apidocs_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class ApidocsController < ActionController::Base
+  include Swagger::Blocks
+
+  swagger_root do
+    key :swagger, '2.0'
+
+    info do
+      key :version, '1.0.0'
+      key :title, 'Bookmarker API'
+      key :description, 'Bookmarker API documentation'
+
+      contact do
+        key :name, 'duetcode.io'
+      end
+    end
+
+    key :host, 'localhost:3000'
+    key :basePath, '/api/v1'
+    key :consumes, ['application/json']
+    key :produces, ['application/json']
+    key :schemes, ['http']
+  end
+
+  # A list of all classes that have swagger_* declarations.
+  SWAGGERED_CLASSES = [
+    self,
+    Swagger::Controllers::UsersController,
+    Swagger::Models::UserInput
+  ].freeze
+
+  def index
+    render json: Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
     end
   end
+
+  resources :apidocs, only: [:index]
 end
